452130c07ea08bd4ad6dc4dfd7bd4f93a355960b
Shravan Kumar Singh <sarvan17@duck.com>
2023-04-07 23:53:09 +0530

    changes

--- content.js
+++ content.js
@@ -1,22 +1,36 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        try {
-            document.getElementById("order_billing_name").value = request.name;
-            document.getElementById("order_email").value = request.email;
-            document.getElementById("order_tel").value = request.telephone;
-            document.getElementById("order_billing_address").value = request.address1;
-            document.getElementById("order_billing_address_2").value = request.address2;
-            document.getElementById("order_billing_address_3").value = request.address3;
-            document.getElementById("order_billing_city").value = request.city;
-            document.getElementById("order_billing_zip").value = request.postcode;
-            document.getElementById("credit_card_number").value = request.cardnumber;
-            document.getElementById('credit_card_month').value = request.month;
-            document.getElementById('credit_card_year').value = request.year;
-            document.getElementById("credit_card_verification_value").value = request.cvv; 
-            sendResponse({status: "Success!"});
-        } catch (error) {
-            console.log(error)
-            sendResponse({status: "Exception occurred!"});
-        }
+        try{
+            var x = myframe.document.querySelectorAll('textarea');
+            var i = 0;
+            for (i = 0; i < x.length; i++){
+                if(x[i].id==="rad_1_11"){
+                    x[i].value=request.rad_1_11;
+                }
+                else if(x[i].id==="rad_1_12"){
+                    x[i].value=request.rad_1_12;
+                }
+                else if(x[i].id==="rad_2_11"){
+                    x[i].value=request.rad_2_11;
+                }
+                else if(x[i].id==="rad_2_12"){
+                    x[i].value=request.rad_2_12;
+                }
+                else{
+                    x[i].value=request.rad_0_15;
+                }
+            }
+          
+            x = myframe.document.querySelectorAll('input[type="radio"]');
+            for (i = 1; i < x.length; i++){
+                if (x[i].getAttribute('name') == 'check'){
+                    continue;
+                }
+                x[i].click();
+            }
+          
+          }catch(err){
+            console.log(err);
+          }
     }
 );
--- ext/script.js
+++ ext/script.js
@@ -2,18 +2,11 @@ document.getElementById("auto-fill").addEventListener("click", () => {
 	/* Auto fill form */
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {
-			name: document.getElementById('name').value,
-			email: document.getElementById('email').value,
-			telephone: document.getElementById('telephone').value,
-			address1: document.getElementById('address1').value,
-			address2: document.getElementById('address2').value,
-			address3: document.getElementById('address3').value,
-			city: document.getElementById('city').value,
-			postcode: document.getElementById('postcode').value,
-			cardnumber: document.getElementById('card-number').value,
-			month: document.getElementById('month').value,
-			year: document.getElementById('year').value,
-			cvv: document.getElementById('cvv').value
+			rad_1_11: document.getElementById('t-st').value,
+			rad_1_12: document.getElementById('t-w').value,
+			rad_2_11: document.getElementById('c-st').value,
+			rad_2_12: document.getElementById('c-w').value,
+			rad_0_15: document.getElementById('sug').value,
 		}, function(response) {
 			console.log(response.status);
 		});
@@ -22,64 +15,39 @@ document.getElementById("auto-fill").addEventListener("click", () => {
 
 document.getElementById("reset-fields").addEventListener("click", () => {
 	/* Reset extension form values */
-	document.getElementById('name').value = '';
-	document.getElementById('email').value = '';
-	document.getElementById('telephone').value = '';
-	document.getElementById('address1').value = '';
-	document.getElementById('address2').value = '';
-	document.getElementById('address3').value = '';
-	document.getElementById('city').value = '';
-	document.getElementById('postcode').value = '';
-	document.getElementById('card-number').value = '';
-	document.getElementById('cvv').value = '';
+	document.getElementById('t-st').value = '';
+	document.getElementById('t-w').value = '';
+	document.getElementById('c-st').value = '';
+	document.getElementById('c-w').value = '';
+	document.getElementById('sug').value = '';
 });
 
 
 document.getElementById("save").addEventListener("click", () => {
 	chrome.storage.sync.set({
-		name: document.getElementById('name').value,
-		email: document.getElementById('email').value,
-		telephone: document.getElementById('telephone').value,
-		address1: document.getElementById('address1').value,
-		address2: document.getElementById('address2').value,
-		address3: document.getElementById('address3').value,
-		city: document.getElementById('city').value,
-		postcode: document.getElementById('postcode').value,
-		cardnumber: document.getElementById('card-number').value,
-		month: document.getElementById('month').value,
-		year: document.getElementById('year').value,
-		cvv: document.getElementById('cvv').value
+		rad_1_11: document.getElementById('t-st').value,
+		rad_1_12: document.getElementById('t-w').value,
+		rad_2_11: document.getElementById('c-st').value,
+		rad_2_12: document.getElementById('c-w').value,
+		rad_0_15: document.getElementById('sug').value,
 	}, function() {
+		document.getElementById('saved').value="Saved!";
 		console.log("Saved!");
 	});
 });
 
 document.getElementById("load").addEventListener("click", () => {
 	chrome.storage.sync.get([
-		'name',
-		'email',
-		'telephone',
-		'address1',
-		'address2',
-		'address3',
-		'city',
-		'postcode',
-		'cardnumber',
-		'month',
-		'year',
-		'cvv'
+		'rad_1_11',
+		'rad_1_12',
+		'rad_2_11',
+		'rad_2_12',
+		'rad_0_15',
 	], function(result) {
-		document.getElementById('name').value = result.name;
-		document.getElementById('email').value = result.email;
-		document.getElementById('telephone').value = result.telephone;
-		document.getElementById('address1').value = result.address1;
-		document.getElementById('address2').value = result.address2;
-		document.getElementById('address3').value = result.address3;
-		document.getElementById('city').value = result.city;
-		document.getElementById('postcode').value = result.postcode;
-		document.getElementById('card-number').value = result.cardnumber;
-		document.getElementById('month').value = result.month,
-		document.getElementById('year').value = result.year,
-		document.getElementById('cvv').value = result.cvv;
+		document.getElementById('t-st').value = result.rad_1_11;
+		document.getElementById('t-w').value = result.rad_1_12;
+		document.getElementById('c-st').value = result.rad_2_11;
+		document.getElementById('c-w').value = result.rad_2_12;
+		document.getElementById('sug').value = result.rad_0_15;
 	});
 });
